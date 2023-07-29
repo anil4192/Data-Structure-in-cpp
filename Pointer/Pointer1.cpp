f2ddda059d59e89f6034bd3b007d1c81933393a6
anil4192 <anilmisal00@gmail.com>
2023-07-29 19:01:24 +0530

    Abc

---
+++ Pointer/Pointer1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void square(int* p)
+{
+    int a = 10;
+    p = &a;
+    *p = (*p) * (*p);
+}
+
+int main()
+{
+    int a = 10;
+    square(&a);
+    cout<<a<<endl; //10
+
+    return 0;
+}
---
+++ Pointer/basics.cpp
@@ -0,0 +1,11 @@
+ #include<iostream>
+ using namespace std;
+ 
+ int main()
+ {
+    int a = 5;
+    int b = 10;
+    cout<<a<<endl;
+    cout<<&a<<endl;
+     return 0; 
+ }
